26b957d152a99315ab6b1d6a4f32f3c0250c2c20
jmgc <chemoki@gmail.com>
2015-04-13 16:05:23 +0200

    Swap between sintba and uintba
    Now uintba is a children class of sintba, to simplify the math between
    them.

--- myhdl/numeric/_sintba.py
+++ myhdl/numeric/_sintba.py
@@ -19,13 +19,29 @@
 
 from __future__ import absolute_import, division
 from ._bitarray import bitarray
-from ._uintba import uintba
 from myhdl._compat import long, integer_types
+from copy import copy
 
 import warnings
 
 
-class sintba(uintba):
+class sintba(bitarray):
+    def __init__(self, *args, **kwargs):
+        value, high, low = self._get_arguments(*args, **kwargs)
+
+        if low is None:
+            if (high is not None):
+                low = getattr(high, 'low', 0)
+                high = getattr(high, 'high', high)
+            else:
+                low = 0
+
+        if low != 0:
+            raise TypeError("The low parameter must be 0 or None " \
+                            "{}".format(low))
+
+        bitarray.__init__(self, value, high, low)
+
     def _from_int(self, value, high, low=0):
         val = long(value)
 
@@ -50,6 +66,16 @@ class sintba(uintba):
 
         self._wrap()
 
+    def _resize(self, value):
+        if value < 0:
+            result = type(self)(-1, self)
+        else:
+            result = type(self)(0, self)
+        bound = min(value.high, result.high)-1
+        if bound > 0:
+            result[bound:] = value[bound:]
+        self._val = result._val
+
     def _get_max(self):
         return (long(1) << (self._high - self._low - 1))
 
@@ -73,7 +99,7 @@ class sintba(uintba):
         val = self._val
         if val < 0:
             val = -val
-        return type(self)(val, high=self)
+        return type(self)(val, self)
 
     def __neg__(self):
         result = type(self)(0, self)
@@ -91,9 +117,6 @@ class sintba(uintba):
         elif isinstance(other, sintba):
             value = other._val
             length = other._high
-        elif isinstance(other, uintba):
-            value = type(self)(other)._val
-            length = other._high
         else:
             return NotImplemented
         
@@ -106,12 +129,9 @@ class sintba(uintba):
 
     def __radd__(self, other):
         if isinstance(other, integer_types):
-            value = type(self)(other, self._high)
-        elif isinstance(other, uintba):
-            value = type(self)(other)
+            return type(self)(other, self) + self
         else:
             return NotImplemented
-        return value + self
 
     def __sub__(self, other):
         length = self._high
@@ -119,9 +139,6 @@ class sintba(uintba):
             value = other
         elif isinstance(other, sintba):
             value = other._val
-            length = other._high
-        elif isinstance(other, uintba):
-            value = type(self)(other)._val
             length = other._high
         else:
             return NotImplemented
@@ -135,12 +152,9 @@ class sintba(uintba):
 
     def __rsub__(self, other):
         if isinstance(other, integer_types):
-            value = type(self)(other, self._high)
-        elif isinstance(other, uintba):
-            value = type(self)(other)
+            return type(self)(other, self) - self
         else:
             return NotImplemented
-        return value - self
 
     def __mul__(self, other):
         length = self._high
@@ -149,35 +163,28 @@ class sintba(uintba):
         elif isinstance(other, sintba):
             value = other._val
             length = other._high
-        elif isinstance(other, uintba):
-            value = type(self)(other)._val
-            length = other._high
         else:
             return NotImplemented
         
         size = self._high + length
         
-        result = type(self)(0, high=size)
+        result = type(self)(0, self)
+        result._high = size
         result._val = self._val * value
         result._wrap()
         return result
 
     def __rmul__(self, other):
         if isinstance(other, integer_types):
-            value = type(self)(other, self._high)
-        elif isinstance(other, uintba):
-            value = type(self)(other, other._high)
+            return type(self)(other, self) * self
         else:
             return NotImplemented
-        return value * self
 
     def __floordiv__(self, other):
         if isinstance(other, integer_types):
             other_value = other
         elif isinstance(other, sintba):
             other_value = other._val
-        elif isinstance(other, uintba):
-            other_value = type(self)(other)._val
         else:
             return NotImplemented
         
@@ -204,12 +211,9 @@ class sintba(uintba):
 
     def __rfloordiv__(self, other):
         if isinstance(other, integer_types):
-            value = type(self)(other, self._high)
-        elif isinstance(other, uintba):
-            value = type(self)(other)
+            return type(self)(other, self) // self
         else:
             return NotImplemented
-        return value // self
 
     def __mod__(self, other):
         length = self._high
@@ -217,9 +221,6 @@ class sintba(uintba):
             value = other
         elif isinstance(other, sintba):
             value = other._val
-            length = other._high
-        elif isinstance(other, uintba):
-            value = type(self)(other)._val
             length = other._high
         else:
             return NotImplemented
@@ -233,9 +234,404 @@ class sintba(uintba):
 
     def __rmod__(self, other):
         if isinstance(other, integer_types):
-            value = type(self)(other, self._high)
-        elif isinstance(other, uintba):
-            value = type(self)(other)
+            return type(self)(other, self) % self
         else:
             return NotImplemented
-        return value % self
+
+    def __lshift__(self, other):
+        if isinstance(other, integer_types):
+            if other < 0:
+                return NotImplemented
+            else:
+                value = other
+        elif isinstance(other, sintba):
+            if other._val < 0:
+                return NotImplemented
+            else:
+                value = other._val
+        else:
+            return NotImplemented
+
+        result = copy(self)
+        result._val = self._val << value
+        result._wrap()
+        return result
+
+    def __rlshift__(self, other):
+        if isinstance(other, integer_types):
+            if self._val < 0:
+                return NotImplemented
+            else:
+                result = copy(self)
+                result._val = other << self._val
+                result._wrap()
+                return result
+        else:
+            return NotImplemented
+
+    def __rshift__(self, other):
+        if isinstance(other, integer_types):
+            if other < 0:
+                return NotImplemented
+            else:
+                value = other
+        elif isinstance(other, sintba):
+            if other._val < 0:
+                return NotImplemented
+            else:
+                value = other._val
+        else:
+            return NotImplemented
+
+        result = copy(self)
+        result._val = self._val >> value
+        result._wrap()
+        return result
+
+    def __rrshift__(self, other):
+        if isinstance(other, integer_types):
+            if self._val < 0:
+                return NotImplemented
+            else:
+                result = copy(self)
+                result._val = other >> self._val
+                result._wrap()
+                return result
+        else:
+            return NotImplemented
+
+    def __and__(self, other):
+        if isinstance(other, sintba):
+            value = other
+        else:
+            return NotImplemented
+
+        return bitarray.__and__(self, value)            
+
+    __rand__ = __and__
+
+    def __or__(self, other):
+        if isinstance(other, sintba):
+            value = other
+        else:
+            return NotImplemented
+
+        return bitarray.__or__(self, value)            
+
+    __ror__ = __or__
+
+    def __xor__(self, other):
+        if isinstance(other, sintba):
+            value = other
+        else:
+            return NotImplemented
+
+        return bitarray.__xor__(self, value)            
+
+    __rxor__ = __xor__
+
+    def __iadd__(self, other):
+        if not isinstance(other, (integer_types, sintba)):
+            return NotImplemented
+        
+        result = self + other
+        if result.high < self._high or result.low > self._low:
+            result = result.resize(self._high, self._low)
+        elif result.high < self._high:
+            result = result.resize(self._high, result.low)
+        elif result.low > self._low:
+            result = result.resize(result.high, self._low)
+        value = result[self._high:self._low]
+        self._val = value._val
+        self._wrap()
+        return self
+
+    def __isub__(self, other):
+        if not isinstance(other, (integer_types, sintba)):
+            return NotImplemented
+        
+        result = self - other
+        if result.high < self._high or result.low > self._low:
+            result = result.resize(self._high, self._low)
+        elif result.high < self._high:
+            result = result.resize(self._high, result.low)
+        elif result.low > self._low:
+            result = result.resize(result.high, self._low)
+        value = result[self._high:self._low]
+        self._val = value._val
+        self._wrap()
+        return self
+
+    def __imul__(self, other):
+        if not isinstance(other, (integer_types, sintba)):
+            return NotImplemented
+        
+        result = self * other
+        if result.high < self._high or result.low > self._low:
+            result = result.resize(self._high, self._low)
+        elif result.high < self._high:
+            result = result.resize(self._high, result.low)
+        elif result.low > self._low:
+            result = result.resize(result.high, self._low)
+        value = result[self._high:self._low]
+        self._val = value._val
+        self._wrap()
+        return self
+
+    def __ifloordiv__(self, other):
+        if not isinstance(other, (integer_types, sintba)):
+            return NotImplemented
+        
+        result = self // other
+        if result.high < self._high or result.low > self._low:
+            result = result.resize(self._high, self._low)
+        elif result.high < self._high:
+            result = result.resize(self._high, result.low)
+        elif result.low > self._low:
+            result = result.resize(result.high, self._low)
+        value = result[self._high:self._low]
+        self._val = value._val
+        self._wrap()
+        return self
+
+    def __imod__(self, other):
+        if not isinstance(other, (integer_types, sintba)):
+            return NotImplemented
+        
+        result = self % other
+        if result.high < self._high or result.low > self._low:
+            result = result.resize(self._high, self._low)
+        elif result.high < self._high:
+            result = result.resize(self._high, result.low)
+        elif result.low > self._low:
+            result = result.resize(result.high, self._low)
+        value = result[self._high:self._low]
+        self._val = value._val
+        self._wrap()
+        return self
+
+    def __ilshift__(self, other):
+        if isinstance(other, integer_types):
+            if other < 0:
+                return NotImplemented
+            else:
+                value = other
+        elif isinstance(other, sintba):
+            if other._val < 0:
+                return NotImplemented
+            else:
+                value = other._val
+        else:
+            return NotImplemented
+
+        self._val = self._val << value
+        self._wrap()
+        return self
+
+    def __irshift__(self, other):
+        if isinstance(other, integer_types):
+            if other < 0:
+                return NotImplemented
+            else:
+                value = other
+        elif isinstance(other, sintba):
+            if other._val < 0:
+                return NotImplemented
+            else:
+                value = other._val
+        else:
+            return NotImplemented
+
+        self._val = self._val >> value
+        self._wrap()
+        return self
+
+    def __iand__(self, other):
+        if isinstance(other, sintba):
+            value = other.resize(self)
+        else:
+            return NotImplemented
+
+        return bitarray.__iand__(self, value)            
+
+    def __ior__(self, other):
+        if isinstance(other, sintba):
+            value = other.resize(self)
+        else:
+            return NotImplemented
+
+        return bitarray.__ior__(self, value)            
+
+    def __ixor__(self, other):
+        if isinstance(other, sintba):
+            value = other.resize(self)
+        else:
+            return NotImplemented
+
+        return bitarray.__ixor__(self, value)            
+
+    def __pos__(self):
+        if self._val < 0:
+            value = -self._val
+        else:
+            value = self._val
+        return type(self)(value, self)
+
+    def __int__(self):
+        return int(self._val)
+
+    def __long__(self):
+        return long(self._val)
+
+    def __float__(self):
+        return float(self._val)
+
+    def __oct__(self):
+        return oct(long(self._val))
+
+    def __hex__(self):
+        return hex(long(self._val))
+
+    def __index__(self):
+        return long(self._val)
+
+    # comparisons
+    def __eq__(self, other):
+        if isinstance(other, integer_types):
+            return self._val == other
+        elif isinstance(other, bitarray):
+            if ((self._low >= other.high) or \
+                    (self._high <= other.low)):
+                return (self._val == 0) and (other.__index__() == 0)
+            else:
+                if other.low < 0:
+                    check = self._val << -other.low
+                    return check == other.__index__()
+                else:
+                    check = other.__index__() << other.low
+                    return check == self._val
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, integer_types):
+            return self._val != other
+        elif isinstance(other, bitarray):
+            if ((self._low >= other.high) or \
+                    (self._high <= other.low)):
+                return (self._val != 0) or (other.__index__() != 0)
+            else:
+                if other.low < 0:
+                    check = self._val << -other.low
+                    return check != other.__index__()
+                else:
+                    check = other.__index__() << other.low
+                    return check != self._val
+        else:
+            return NotImplemented
+
+
+    def __lt__(self, other):
+        if isinstance(other, integer_types):
+            return self._val < other
+        elif isinstance(other, bitarray):
+            if self._low >= other.high:
+                return self._val < 0
+            elif self._high <= other.low:
+                return 0 < other.__index__()
+            else:
+                if other.low < 0:
+                    check = self._val << -other.low
+                    return check < other.__index__()
+                else:
+                    check = other.__index__() << other.low
+                    return self._val < check
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, integer_types):
+            return self._val <= other
+        elif isinstance(other, bitarray):
+            if self._low >= other.high:
+                return self._val <= 0
+            elif self._high <= other.low:
+                return 0 <= other.__index__()
+            else:
+                if other.low < 0:
+                    check = self._val << -other.low
+                    return check <= other.__index__()
+                else:
+                    check = other.__index__() << other.low
+                    return self._val <= check
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, integer_types):
+            return self._val > other
+        elif isinstance(other, bitarray):
+            if self._low >= other.high:
+                return self._val > 0
+            elif self._high <= other.low:
+                return 0 > other.__index__()
+            else:
+                if other.low < 0:
+                    check = self._val << -other.low
+                    return check > other.__index__()
+                else:
+                    check = other.__index__() << other.low
+                    return self._val > check
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, integer_types):
+            return self._val >= other
+        elif isinstance(other, bitarray):
+            if self._low >= other.high:
+                return self._val >= 0
+            elif self._high <= other.low:
+                return 0 >= other.__index__()
+            else:
+                if other.low < 0:
+                    check = self._val << -other.low
+                    return check >= other.__index__()
+                else:
+                    check = other.__index__() << other.low
+                    return self._val >= check
+        else:
+            return NotImplemented
+
+
+    # representation
+    def __str__(self):
+        return str(self._val)
+
+    def __repr__(self):
+        return type(self).__name__ + \
+                "({}, high={})".format(self._val, self._high)
+
+    def resize(*args):
+        length = len(args)
+        if length == 2:
+            value = args[0]
+            format = args[1]
+            if isinstance(format, integer_types):
+                high = format
+                low = 0
+            else:
+                high = format.high
+                low = format.low
+        elif length == 3:
+            value = args[0]
+            high = args[1]
+            low = args[2]
+        else:
+            raise TypeError("Incorrect number of arguments")
+        result = copy(value)
+        result._high = high
+        result._low = low
+        result._resize(value)
+        result._wrap()
+        return result
