d10624a535ffdc91693cc3b63385978cb28698c8
Jose M. Gomez <jm.gomez@ub.edu>
2016-02-28 23:20:36 +0100

    Solved issue 134

--- myhdl/test/bugs/test_issue134.py
+++ myhdl/test/bugs/test_issue134.py
@@ -30,7 +30,6 @@ def issue_134(ab_in, ab_out):
     return inverta, invertb
 
 
-@pytest.mark.xfail
 def test_issue_134():
     """ check for port name collision"""
     assert analyze(issue_134, AB(), AB()) == 0
