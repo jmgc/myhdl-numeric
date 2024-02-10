7dc5ebdfcdb76295e225f74e22b2dc79316f99b8
jmgc <chemoki@gmail.com>
2024-02-10 17:51:05 +0100

    Update test_uintba.py
    Changes associated with the python 2.7.

--- myhdl/test/numeric/test_uintba.py
+++ myhdl/test/numeric/test_uintba.py
@@ -222,8 +222,8 @@ class TestUIntBaIndexing(TestCase):
                             ba[i:j] = val
                         except RuntimeWarning:
                             if isinstance(val, int):
-                                self.assertTrue((bit_length(val) > (i - j)) \
-                                                or (bit_length(-1 - val) > \
+                                self.assertTrue((val.bit_length() > (i - j)) \
+                                                or ((-1 - val).bit_length() > \
                                                     (i - j)))
                             else:
                                 self.assertTrue((len(val) != (i - j)) or \
@@ -253,9 +253,9 @@ class TestUIntBaIndexing(TestCase):
                         bai[:j] = wrap(1 - val - 2, bai[:j])  # Workaraound for -1 - val
                     except RuntimeWarning:
                         if isinstance(val, int):
-                            self.assertTrue((bit_length(val) > \
+                            self.assertTrue((val.bit_length() > \
                                              (len(ba) - j)) or \
-                                            (bit_length(-1 - val) > \
+                                            ((-1 - val).bit_length() > \
                                              (len(bai) - j - 1)))
                         else:
                             self.assertTrue((len(val) != (len(ba) - j)) or \
@@ -579,7 +579,7 @@ class TestUIntBaBounds(TestCase):
             try:
                 a[:] = v
             except RuntimeWarning:
-                self.assertTrue(bit_length(v) > len(a))
+                self.assertTrue(v.bit_length() > len(a))
         warnings.resetwarnings()
 
     def checkBounds(self, i, j, op):
