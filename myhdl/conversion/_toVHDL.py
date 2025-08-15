ec122cd5df78c86be48720fa431e7b21b6c5d66f
jmgc <jm.gomez@ub.edu>
2025-08-15 07:49:41 +0200

    Solved issues with std_logic_ports
    Revised the behaviour of std_logic_ports and added tests.

--- myhdl/conversion/_toVHDL.py
+++ myhdl/conversion/_toVHDL.py
@@ -597,12 +597,12 @@ class _GenerateHierarchy(object):
             elif isinstance(s, _MemInfo):
                 ds = s.elObj
 
-            if stdLogicPorts and ds._type in (intbv, bitarray):
+            if stdLogicPorts and ds._type in (intbv, bitarray, uintba, sintba, sfixba):
                 final_name = port.name + "_num"
                 convert_port = port.name
 
                 if isinstance(port.vhd_type, vhd_array):
-                    vhd_obj = vhd_array(s.depth, vhd_vector(port.vhd_type.type.size))
+                    vhd_obj = vhd_array(s.depth, vhd_vector(len(port.vhd_type.type)))
                     vhd_ports_convert.append(vhd_signal(port.name, port.signal,
                                                         port.vhd_type, True))
                 else:
@@ -1583,14 +1583,14 @@ def _writePort(f, port, entity=True):
         if isinstance(port.vhd_type, (vhd_array, vhd_enum)):
             for sl in port.signal.elObj._slicesigs:
                 sl._setName('VHDL')
-            if isinstance(port.vhd_type.type, vhd_vector):
-                port_type = vhd_array(port.vhd_type.high + 1, vhd_vector(port.vhd_type.type.size)).toStr(False)
+            if isinstance(port.vhd_type.type, (vhd_vector, vhd_unsigned, vhd_signed, vhd_sfixed)):
+                port_type = vhd_array(port.vhd_type.high + 1, vhd_vector(len(port.vhd_type.type))).toStr(False)
             else:
                 port_type = "std_logic_vector"
         else:
             for sl in port.signal._slicesigs:
                 sl._setName('VHDL')
-            port_type = "std_logic_vector(%d downto 0)" % (port.vhd_type.size - 1)
+            port_type = "std_logic_vector(%d downto 0)" % (len(port.vhd_type) - 1)
 
     if port.direction == "in" or port.internal is None or (not port.init_signals) or isinstance(port.vhd_type,
                                                                                                 vhd_array):
@@ -1607,21 +1607,39 @@ def _writePort(f, port, entity=True):
         port_conversions = port.entity.architecture.signal_conversions
         if port.direction in ("inout", "out"):
             if isinstance(port.vhd_type, vhd_array):
-                for idx in range(port.vhd_type.high + 1):
-                    port_conversions.append(vhd_assign(f"{port.name:s}({idx:d})",
-                                                       f"std_logic_vector({port.convert}({idx}))"))
+                if isinstance(port.vhd_type.type, vhd_sfixed):
+                    for idx in range(port.vhd_type.high + 1):
+                        port_conversions.append(vhd_assign(f"{port.name:s}({idx:d})",
+                                                           f"to_slv({port.convert}({idx}))"))
+                else:
+                    for idx in range(port.vhd_type.high + 1):
+                        port_conversions.append(vhd_assign(f"{port.name:s}({idx:d})",
+                                                           f"std_logic_vector({port.convert}({idx}))"))
             else:
-                port_conversions.append(vhd_assign(port.name,
-                                                   f"std_logic_vector({port.convert})"))
+                if isinstance(port.vhd_type, vhd_sfixed):
+                    port_conversions.append(vhd_assign(port.name,
+                                                       f"to_slv({port.convert})"))
+                else:
+                    port_conversions.append(vhd_assign(port.name,
+                                                       f"std_logic_vector({port.convert})"))
             port.signal._read = True
         else:
             if isinstance(port.vhd_type, vhd_array):
-                for idx in range(port.vhd_type.high + 1):
-                    port_conversions.append(vhd_assign(port.convert,
-                                                       f"{port.vhd_type.toStr(False)}({port.name}({idx}))"))
+                if isinstance(port.vhd_type.type, vhd_sfixed):
+                    for idx in range(port.vhd_type.high + 1):
+                        port_conversions.append(vhd_assign(f"{port.convert}({idx})",
+                                                           f"to_sfixed({port.name}({idx}), {port.vhd_type.type.size[0]}, {port.vhd_type.type.size[1]})"))
+                else:
+                    for idx in range(port.vhd_type.high + 1):
+                        port_conversions.append(vhd_assign(f"{port.convert}({idx})",
+                                                           f"{port.vhd_type.type.toStr(False)}({port.name}({idx}))"))
             else:
-                port_conversions.append(vhd_assign(port.convert,
-                                                   f"{port.vhd_type.toStr(False)}({port.name})"))
+                if isinstance(port.vhd_type, vhd_sfixed):
+                    port_conversions.append(vhd_assign(port.convert,
+                                                       f"to_sfixed({port.name}, {port.vhd_type.size[0]}, {port.vhd_type.size[1]})"))
+                else:
+                    port_conversions.append(vhd_assign(port.convert,
+                                                       f"{port.vhd_type.toStr(False)}({port.name})"))
             port.signal._driven = "reg"
 
 
@@ -1777,25 +1795,53 @@ def _writeCompUnits(f, entity):
                 if convert:
                     port_name = port.name
                 name = component.ports_signals_dict[port_name]
-                if convert and isinstance(port.vhd_type, (vhd_unsigned, vhd_signed, vhd_sfixed)):
+                if convert and isinstance(port.vhd_type, (vhd_unsigned, vhd_signed)):
                     f.write(c)
                     if port.direction == "out":
-                        f.write("%s(%s) => %s" % (port.vhd_type.toStr(False), port_name, name))
+                        if name == 'open':
+                            f.write("%s => %s" % (port_name, name))
+                        else:
+                            f.write("%s(%s) => %s" % (port.vhd_type.toStr(False), port_name, name))
                     elif port.direction == "in":
                         f.write("%s => std_logic_vector(%s)" % (port_name, name))
                     else:
                         f.write("%s(%s) => std_logic_vector(%s)" % (port.vhd_type.toStr(False), port_name, name))
-                elif convert and isinstance(port.vhd_type, vhd_array):
-                    for idx in range(port.vhd_type.high + 1):
-                        f.write(c)
-                        if port.direction == "out":
-                            f.write(
-                                "%s(%s(%d)) => %s(%d)" % (port.vhd_type.type.toStr(False), port_name, idx, name, idx))
-                        elif port.direction == "in":
-                            f.write("%s(%d) => std_logic_vector(%s(%d))" % (port_name, idx, name, idx))
+                elif convert and isinstance(port.vhd_type, (vhd_sfixed)):
+                    f.write(c)
+                    if port.direction == "out":
+                        if name == 'open':
+                            f.write("%s => %s" % (port_name, name))
                         else:
-                            f.write("%s(%s(%d)) => std_logic_vector(%s(%d))" % (
-                                port.vhd_type.type.toStr(False), port_name, idx, name, idx))
+                            f.write("%s(%s) => %s" % (port.vhd_type.toStr(False), port_name, name))
+                    elif port.direction == "in":
+                        f.write("%s => to_slv(%s)" % (port_name, name))
+                    else:
+                        f.write("%s(%s) => to_slv(%s)" % (port.vhd_type.toStr(False), port_name, name))
+
+                elif convert and isinstance(port.vhd_type, vhd_array):
+                    if isinstance(port.vhd_type.type, vhd_sfixed):
+                        for idx in range(port.vhd_type.high + 1):
+                            f.write(c)
+                            if port.direction == "out":
+                                f.write(
+                                    "%s(%s(%d)) => %s(%d)" % (port.vhd_type.type.toStr(False), port_name, idx, name,
+                                                              idx))
+                            elif port.direction == "in":
+                                f.write("%s(%d) => to_slv(%s(%d))" % (port_name, idx, name, idx))
+                            else:
+                                f.write("%s(%s(%d)) => to_slv(%s(%d))" % (
+                                    port.vhd_type.type.toStr(False), port_name, idx, name, idx))
+                    else:
+                        for idx in range(port.vhd_type.high + 1):
+                            f.write(c)
+                            if port.direction == "out":
+                                f.write(
+                                    "%s(%s(%d)) => %s(%d)" % (port.vhd_type.type.toStr(False), port_name, idx, name, idx))
+                            elif port.direction == "in":
+                                f.write("%s(%d) => std_logic_vector(%s(%d))" % (port_name, idx, name, idx))
+                            else:
+                                f.write("%s(%s(%d)) => std_logic_vector(%s(%d))" % (
+                                    port.vhd_type.type.toStr(False), port_name, idx, name, idx))
                 else:
                     f.write(c)
                     f.write("%s => %s" % (port_name, name))
--- myhdl/test/conversion/numeric/test_no_instances.py
+++ myhdl/test/conversion/numeric/test_no_instances.py
@@ -1,6 +1,4 @@
-import pytest
-from myhdl import uintba, sintba, Signal, instance, delay, conversion, ExtractHierarchyError
-from myhdl.conversion._misc import _error as errors
+from myhdl import uintba, sintba, sfixba, Signal, instance, delay, conversion, ExtractHierarchyError, always_comb
 
 
 def check_no_instances():
@@ -18,6 +16,7 @@ def check_no_instances():
 
     return returned
 
+
 def check_second_order():
     a = Signal(uintba(23, 8))
 
@@ -33,6 +32,162 @@ def check_second_order():
 
 def first_order():
     second = check_second_order()
+
+
+def std_logic_ports_component(check: Signal, input_port_c: Signal, output_port_c: Signal,
+                              input_port_not_used: Signal, output_port_not_used: Signal):
+    @always_comb
+    def logic_loop():
+        if check:
+            output_port_c.next = input_port_c
+            output_port_not_used.next = input_port_not_used
+
+    return logic_loop
+
+
+def std_logic_ports_entity(check: Signal, input_port_e: Signal, output_port_e: Signal):
+    input_signal = Signal(uintba(0, 16))
+    output_signal = Signal(uintba(0, 16))
+    component = std_logic_ports_component(check, input_port_e, output_port_e, input_signal, output_signal)
+    return component
+
+
+def std_logic_ports_test_sfixba_bench():
+    check_signal = Signal(False)
+    input_signal = Signal(sfixba(0, 16, -16))
+    output_signal = Signal(sfixba(0, 16, -16))
+
+    entity = std_logic_ports_entity(check_signal, input_signal, output_signal)
+
+    @instance
+    def stimulus():
+        for i in range(5):
+            check_signal.next = not check_signal
+            input_signal.next = i / 10.0
+            yield delay(20)
+            assert (check_signal and (input_signal == output_signal)) or (
+                        not check_signal and (output_signal != input_signal))
+            print(f"Input: {input_signal}, Output: {output_signal}")
+
+    return entity, stimulus
+
+
+def test_std_logic_ports_sfixba():
+    tmp_val = conversion.toVHDL.std_logic_ports
+    conversion.toVHDL.std_logic_ports = True
+    assert conversion.verify(std_logic_ports_test_sfixba_bench) == 0
+    conversion.toVHDL.std_logic_ports = tmp_val
+
+
+def std_logic_ports_test_intba_bench():
+    check_signal = Signal(False)
+    input_signal = Signal(uintba(0, 16))
+    output_signal = Signal(sintba(0, 16))
+
+    entity = std_logic_ports_entity(check_signal, input_signal, output_signal)
+
+    @instance
+    def stimulus():
+        for i in range(5):
+            check_signal.next = not check_signal
+            input_signal.next = i
+            yield delay(20)
+            assert (check_signal and (input_signal == output_signal)) or (
+                        not check_signal and (output_signal != input_signal))
+            print(f"Input: {input_signal}, Output: {output_signal}")
+
+    return entity, stimulus
+
+
+def test_std_logic_ports_intba():
+    tmp_val = conversion.toVHDL.std_logic_ports
+    conversion.toVHDL.std_logic_ports = True
+    assert conversion.verify(std_logic_ports_test_intba_bench) == 0
+    conversion.toVHDL.std_logic_ports = tmp_val
+
+
+def std_logic_ports_component_array(check: Signal, input_port_c: list, output_port_c: list,
+                                    input_port_not_used: Signal, output_port_not_used: Signal):
+    @always_comb
+    def logic_loop():
+        if check:
+            for i in range(len(input_port_c)):
+                output_port_c[i].next = input_port_c[i]
+            output_port_not_used.next = input_port_not_used
+
+    return logic_loop
+
+
+def std_logic_ports_entity_array(check: Signal, input_port_e: list, output_port_e: list):
+    input_signal = Signal(uintba(0, 16))
+    output_signal = Signal(uintba(0, 16))
+    components = [std_logic_ports_component(check, input_port_e[idx], output_port_e[idx], input_signal, output_signal)
+                  for idx in range(len(input_port_e))]
+    return components
+
+
+def std_logic_ports_test_sfixba_array_bench():
+    PORTS = 2
+    check_signal = Signal(False)
+    input_signal = [Signal(sfixba(0, 16, -16)) for _ in range(PORTS)]
+    output_signal = [Signal(sfixba(0, 16, -16)) for _ in range(PORTS)]
+
+    entity = std_logic_ports_entity_array(check_signal, input_signal, output_signal)
+
+    @instance
+    def stimulus():
+        for i in range(5):
+            check_signal.next = not check_signal
+            for idx in range(PORTS):
+                input_signal[idx].next = i / 10.0
+            yield delay(20)
+            for idx in range(PORTS):
+                assert (check_signal and (input_signal[idx] == output_signal[idx])) or (
+                            not check_signal and (output_signal[idx] != input_signal[idx]))
+            a = input_signal[0]
+            b = output_signal[0]
+            print(f"Input: {a}, Output: {b}")
+
+    return entity, stimulus
+
+
+def test_std_logic_ports_sfixba_array():
+    tmp_val = conversion.toVHDL.std_logic_ports
+    conversion.toVHDL.std_logic_ports = True
+    assert conversion.verify(std_logic_ports_test_sfixba_array_bench) == 0
+    conversion.toVHDL.std_logic_ports = tmp_val
+
+
+def std_logic_ports_test_intba_array_bench():
+    PORTS = 2
+    check_signal = Signal(False)
+    input_signal = [Signal(sintba(0, 16)) for _ in range(PORTS)]
+    output_signal = [Signal(uintba(0, 16)) for _ in range(PORTS)]
+
+    entity = std_logic_ports_entity_array(check_signal, input_signal, output_signal)
+
+    @instance
+    def stimulus():
+        for i in range(5):
+            check_signal.next = not check_signal
+            for idx in range(PORTS):
+                input_signal[idx].next = i
+            yield delay(20)
+            for idx in range(PORTS):
+                assert (check_signal and (input_signal[idx] == output_signal[idx])) or (
+                            not check_signal and (output_signal[idx] != input_signal[idx]))
+            a = input_signal[0]
+            b = output_signal[0]
+            print(f"Input: {a}, Output: {b}")
+
+    return entity, stimulus
+
+
+def test_std_logic_ports_intba_array():
+    tmp_val = conversion.toVHDL.std_logic_ports
+    conversion.toVHDL.std_logic_ports = True
+    assert conversion.verify(std_logic_ports_test_intba_array_bench) == 0
+    conversion.toVHDL.std_logic_ports = tmp_val
 
 
 def test_no_instances():
